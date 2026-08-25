129165922520983e6f95f347b8efb2fe3bf57ee0
Victor Martins Torres <clerossephirot@gmail.com>
2026-08-24 21:07:07 -0300

    chore: leituraRoutes.js implementado

---
+++ api/src/routes/leituraRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const LeituraRepository = require("../repositories/LeituraRepository");
+const LeituraController = require("../controllers/leituraController");
+
+function criarRotasLeitura(db) {
+  const router = express.Router();
+
+  // Monta a cadeia de dependências: Repository -> Controller
+  const repository = new LeituraRepository(db);
+  const controller = new LeituraController(repository);
+
+  router.post("/", controller.criar.bind(controller));
+
+  return router;
+}
+
+module.exports = criarRotasLeitura;
